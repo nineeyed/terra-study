3a3f2179259834bacbbc1dd65a408830689e9f3b
nineeyed <91732764+nineeyed@users.noreply.github.com>
2022-02-04 16:56:47 +0300

    Added Variables
    Added input and output variables

--- main.tf
+++ main.tf
@@ -1,30 +1,41 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0fd1481c9925c13d3"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-    tags = {
-        Name = "terraform-example"
-    }
+  ami = "ami-0fd1481c9925c13d3"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  tags = {
+    Name = "terraform-example"
+  }
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+  name = "terraform-example-instance"
 
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
 
